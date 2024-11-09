39f612852e3ce1a00224a7e3ffd314489c96ea66
arif noviarrizal <muhammad.noviarrizal@ist.id>
2024-11-09 19:31:24 +0700

    initial project

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import type { Metadata } from "next";
+import "./globals.css";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: "StoreIt",
+  description: "StoreIt - The only storage solution you need.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
